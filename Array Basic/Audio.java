94bad9b711ca10ab6e363881f176d5cfa5cc556d
ritikmishra7488@gmail.com <ritikmishra7488@gmail.com>
2026-01-11 11:03:16 +0530

    First Java Program call Other Class Audio &  Video

---
+++ Array Basic/Audio.java
@@ -0,0 +1,6 @@
+public class Audio {
+    static int sizeAudio=120;
+    public static void play(){
+        System.out.println("Audio is being played");
+    }
+}
---
+++ Array Basic/Video.java
@@ -0,0 +1,7 @@
+public class Video {
+    static int size=720;
+    public static void play(){
+        System.out.println("Video is being played");
+    }
+}
+
