34981c84fd0d229f5223d79f460746a728703864
ritikmishra7488@gmail.com <ritikmishra7488@gmail.com>
2026-01-11 10:29:28 +0530

    Array Basics Program

---
+++ Array Basic/ArrBasics.java
@@ -0,0 +1,11 @@
+class ArrBasics{
+    public static void main (String[]args){
+        int a[]={10,20,30,40,50};
+        System.out.println(a.length);
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[a.length-1]);
+        System.out.println(a[a.length]);        
+    }
+}
